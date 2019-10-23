2c85a101879dc5e76adef22b5a09436c11bb738c
metalnodeug <metalnodeug@gmail.com>
2019-10-23 21:45:58 +0200

    adding method startGame, extension file, bundle.url

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -12,21 +12,48 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
-
-
+    
+    
     var body: some View {
         NavigationView {
             VStack{
-                TextField("Enter your word", text: $newWord)
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
                     .padding()
-
+                
                 List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
                     Text($0)
                 }
             }
             .navigationBarTitle(rootWord)
+            .onAppear(perform: startGame)
         }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else {
+            return
+        }
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle.")
     }
 }
 
