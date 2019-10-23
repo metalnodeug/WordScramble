d67f376e6675a8afe534134d7fe4ce86dd305e73
metalnodeug <metalnodeug@gmail.com>
2019-10-23 13:23:23 +0200

    adding more beautiful interface for textField

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -12,12 +12,14 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
-    
-    
+
+
     var body: some View {
         NavigationView {
             VStack{
                 TextField("Enter your word", text: $newWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
                 
                 List(usedWords, id: \.self) {
                     Text($0)
