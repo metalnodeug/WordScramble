66b0d21431746f541e6b19dafaecd53a79119180
metalnodeug <metalnodeug@gmail.com>
2019-10-23 22:01:17 +0200

    adding error and alert after check the word entry

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -12,8 +12,11 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
-    
-    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+
+
     var body: some View {
         NavigationView {
             VStack{
@@ -21,7 +24,7 @@ struct ContentView: View {
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .autocapitalization(.none)
                     .padding()
-                
+
                 List(usedWords, id: \.self) {
                     Image(systemName: "\($0.count).circle")
                     Text($0)
@@ -29,31 +32,90 @@ struct ContentView: View {
             }
             .navigationBarTitle(rootWord)
             .onAppear(perform: startGame)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
         }
     }
-    
+
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        
+
         guard answer.count > 0 else {
             return
         }
-        
+
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not possible", message: "That isn't a real word.")
+            return
+        }
+
         usedWords.insert(answer, at: 0)
         newWord = ""
     }
-    
+
+    ///Starting game with start.txt file containing all word to the game.
     func startGame() {
-        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
-            if let startWords = try? String(contentsOf: startWordsURL) {
-                let allWords = startWords.components(separatedBy: "\n")
-                
-                rootWord = allWords.randomElement() ?? "silkworm"
-                return
+        guard let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") else {
+            return
+
+        }
+
+        if let startWords = try? String(contentsOf: startWordsURL) {
+            let allWords = startWords.components(separatedBy: "\n")
+
+            rootWord = allWords.randomElement() ?? "silkworm"
+            return
+        }
+
+        fatalError("Could not load start.txt from bundle.")
+    }
+
+
+    ///Check if the word is original.
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+
+    ///Check if the word is possible, this method check each letter in the orignal word and delete each letter after he found an mirror. If all letter are in the word, the func return true, else he return false
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
             }
         }
-        
-        fatalError("Could not load start.txt from bundle.")
+
+        return true
+    }
+
+    /// Using Objec-c UITextChecker to check if the word exist.
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+
+    /// Just create an error to display in .alert
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
